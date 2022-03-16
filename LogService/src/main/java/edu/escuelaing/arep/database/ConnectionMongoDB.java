23dfecbf1eec235048fd875d2920dd590dc36634
Zuly Vargas <zuly.vargas@mail.escuelaing.edu.co>
2022-03-16 08:11:12 -0500

    Base de datos insertar y obtener mensajes

--- LogService/src/main/java/edu/escuelaing/arep/database/ConnectionMongoDB.java
+++ LogService/src/main/java/edu/escuelaing/arep/database/ConnectionMongoDB.java
@@ -2,10 +2,15 @@ package edu.escuelaing.arep.database;
 
 
 import com.mongodb.MongoClientURI;
-import com.mongodb.ServerAddress;
-import com.mongodb.MongoCredential;
-import com.mongodb.MongoClientOptions;
-import com.mongodb.client.MongoClient;
+import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import edu.escuelaing.arep.entry.Message;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 
 /**
@@ -18,13 +23,51 @@ public class ConnectionMongoDB {
     MongoClient mongoClient;
 
 
-
-
+    /**
+     * Create the connection to the database.
+     */
     public ConnectionMongoDB(){
         //https://programmerclick.com/article/8253664516/
 
         uri = new MongoClientURI("mongodb+srv://admin:admin@cluster0.ko7ci.mongodb.net/messages?retryWrites=true&w=majority");
+        mongoClient = new MongoClient(uri);
+    }
+    /**
+     * Inserts a message in the database.
+     * @param message - message to insert.
+     */
+    public void insertMessage(Message message){
+        mongoClient = new MongoClient(uri);
+        MongoDatabase db = mongoClient.getDatabase("lab4");
+        MongoCollection<Document> collection = db.getCollection("messages");
+        Document  document =new Document();
+        document.put("info",message.getContent());
+        document.put("date",message.getDate());
+        collection.insertOne(document);
+    }
 
+    /**
+     * Get and return last 10 messages
+     * @return messagesToShow- last 10 messages.
+     */
+    public ArrayList<Message> getTenMessages() {
+        MongoDatabase database = mongoClient.getDatabase("lab4");
+        MongoCollection<Document> collection = database.getCollection("messages");
+        FindIterable<Document> findIterable = collection.find();
+        ArrayList<Document> documents = new ArrayList<Document>();
+        ArrayList<Message> messages = new ArrayList<Message>();
+        findIterable.into(documents);
+        for (Document doc : documents) {
+            if (doc.get("info") != null && doc.get("date") != null) {
+                messages.add(new Message((String) doc.get("info"), (Date) doc.get("date")));
+            }
+        }
+        ArrayList<Message> messagesToShow = new ArrayList<Message>();
+
+        for (int count = 10 ;  count <= 10; count-- ){
+            if (count < messages.size() )messagesToShow.add(messages.get(count));
+        }
+        return messagesToShow;
     }
 
 
