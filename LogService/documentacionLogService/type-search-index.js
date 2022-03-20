51095cb0522e831bbb84205bd80008f33c32c306
Zuly Vargas <zuly.vargas@mail.escuelaing.edu.co>
2022-03-20 15:29:49 -0500

    Javadoc: Documentacion

---
+++ LogService/documentacionLogService/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"edu.escuelaing.arep","l":"App"},{"p":"edu.escuelaing.arep.database","l":"ConnectionMongoDB"},{"p":"edu.escuelaing.arep.entry","l":"Message"}]
